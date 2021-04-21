ca99a988f48b60b9b3b416fe029a27258ab0b923
Seri Namsri <>
2021-04-21 14:50:44 +0700

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import User from "./components/User";
+import Clock from "./components/Clock";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+        <ul>
+          <User info="User 01" age="30" />
+          <User info="User 02" />
+          <User info="User 03" />
+        </ul>
+        <Clock title="Current data naja = " />
+      </div>
   );
 }
 
---
+++ my-app/src/components/User.js
@@ -0,0 +1,9 @@
+function User(props){
+    return (
+        <li>
+          {props.info}-{props.age}
+        </li>
+      );
+}
+
+export default User;
